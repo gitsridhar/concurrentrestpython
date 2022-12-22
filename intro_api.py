7514a1fb91fc6a78b69d711d65faccf7e2b3c1f4
Sridhar Venkat <sridcloud@gmail.com>
2022-12-22 14:24:43 -0500

    Added sleep to the run_task to make it slower
    Signed-off-by: Sridhar Venkat <sridcloud@gmail.com>

--- intro_api.py
+++ intro_api.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
 from waitress import serve
 from flask_cors import CORS
+import time
+
+from multiprocessing import Process
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 def run_task(body_job_id, body_operator) :
+    time.sleep(10)
     return "Success"
 
 def is_valid_request(body_job_id, body_operator) :
@@ -17,8 +21,10 @@ def task(job_id):
     body_operator = job_id
     if is_valid_request(body_job_id, body_operator) == False :
         return "Bad request, invalid job_id: %s or operator: %s" % (body_job_id, body_operator), 401
-    output = run_task(body_job_id, body_operator)
-    return job_id, 201
+    task_cb = Process(target=run_task, args=(body_job_id, body_operator))
+    task_cb.start()
+
+    return job_id, 202
 
 @app.route('/', methods=['GET'])
 def hello_world():
